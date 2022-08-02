196e8c555ecf10faf2f5885c8b749c31fcf0ecc5
zlop-sudo <2215127316@qq.com>
2022-08-01 22:19:13 -0400

    project init

---
+++ freelancerPlatform/src/main/java/com/flag/freelancerplatform/FreelancerPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.flag.freelancerplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelancerPlatformApplication.class, args);
+    }
+
+}
---
+++ freelancerPlatform/src/test/java/com/flag/freelancerplatform/FreelancerPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flag.freelancerplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
